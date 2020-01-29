c2972fc17828227a2f7d96ed4cd42b8a57a0ae59
nick <nick@163.com>
2020-01-30 00:28:14 +0800

    Add Circle CI status

--- src/test/karma-shared.conf.js
+++ src/test/karma-shared.conf.js
@@ -50,7 +50,7 @@ module.exports = function (config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     // browsers: ['Chrome', 'Firefox', 'Safari', 'IE'],
     // browsers: ['Chrome', 'Firefox', 'Safari'],
-    browsers: ['Chrome'],
+    browsers: ['Chrome', 'Firefox', 'Safari'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
@@ -58,6 +58,6 @@ module.exports = function (config) {
 
     // Concurrency level
     // how many browser should be started simultaneous
-    concurrency: 1
+    concurrency: 2
   })
 }
